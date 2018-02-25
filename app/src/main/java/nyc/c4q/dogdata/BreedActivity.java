bee05f4e398fbef10e011bde5198c7d3366eb76b
bRad-95 <bryantramirez@ac.c4q.nyc>
2018-02-25 12:25:33 -0500

    loginComplete

--- app/src/main/java/nyc/c4q/dogdata/BreedActivity.java
+++ app/src/main/java/nyc/c4q/dogdata/BreedActivity.java
@@ -1,14 +1,24 @@
 package nyc.c4q.dogdata;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class BreedActivity extends AppCompatActivity {
+
+  private TextView welcomeText;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Bundle extras = getIntent().getExtras();
+
+    welcomeText = findViewById(R.id.welcome_tv);
+
+
   }
 }
 
